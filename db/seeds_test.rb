97e1d7d951f47ed5033f8cac6b2d8177dadfd760
Tom <tomecrepont@gmail.com>
2021-02-15 19:07:02 +0100

    Try seeding

---
+++ db/seeds_test.rb
@@ -0,0 +1,21 @@
+
+require 'open-uri'
+require 'nokogiri'
+
+ url = "https://www.akewatu.fr/surf/planches-de-surf?topProduct=0&pricerange=119%3B2000&zone%5B%5D=1&page=5"
+ html_file = open(url).read
+ html_doc = Nokogiri::HTML(html_file)
+ cards = html_doc.search('.catalog__result__cart-item')
+ cards.each do |card| 
+  title = card.search('.title.u-m-t-10.u-p-l-10.u-p-r-10')
+  spec = card.search('.specs.u-m-t-10')
+  
+  if title.count == 1 && spec.count == 1
+    puts title[0].text.strip
+    puts "length: #{spec.search('span')[0].text.strip}"
+    puts "width: #{spec.search('span')[1].text.strip}"
+    puts "thickness: #{spec.search('span')[2].text.strip}"
+    puts "volume: #{spec.search('span')[3].text.strip}"
+  end
+
+end
