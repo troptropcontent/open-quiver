4f759b868dddff2eb681280d47f2a4e756c1d40a
Robyn Perrin <robynperrin76@gmail.com>
2021-02-15 16:50:19 +0100

    Done routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boards do
+    resources :reservations, only: [ :new, :create ]
+  end
+
+  resources :users, only: [ :show ] do
+    resources :reservations, only: [ :show, :index ]
+  end
+
+  resources :reservations, only: [ :update, :edit ] do
+    resources :reviews, only: [ :create ]
+  end
 end
