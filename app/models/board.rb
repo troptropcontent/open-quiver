638dab17c2952a6e70ef1bea45b92105fda16b73
Tom <tomecrepont@gmail.com>
2021-02-16 17:09:08 +0100

    Adding two column to the board model CATEGORY and Description

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ApplicationRecord
+  CATEGORY = ["shortboard", "hybride", "longboard", "fish", "egg", "enfant", "stand up paddle", "mini malibu", "évolutive", "gun", "foil", "planche en mousse", "mid-length", "évolutive / hybride", "deco", "vintage", "retro"]
   has_one_attached :photo
   belongs_to :user
+  validates :category, :inclusion=> { in: CATEGORY}
 end
